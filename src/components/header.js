4e94c701c37688b42ba96b0a467fea670e9fc7a2
grigoar <grigore.nath.j@gmail.com>
2021-09-16 07:02:17 +0300

    addes some new compoentns and pages for the overall picture of the projects

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Scoala de soferi Nath Grigore</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/blog.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const BlogPage = ()=>{
+    return <div>The Blog page</div>
+
+}
+
+export default BlogPage
