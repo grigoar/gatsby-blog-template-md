6756f7d45937afe8fd7c143398de924be6fa5867
grigoar <grigore.nath.j@gmail.com>
2021-09-17 06:51:41 +0300

    added sass and css for styling the themes

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 const Header = () => {
+  const [theme, setTheme] = useState({ light: "light", dark: "", orient: "" })
+
+  useEffect(() => {
+    document.body.className = `${theme.light} ${theme.dark} ${theme.orient}`
+  }, [theme])
+  const themeDark = () => {
+    setTheme({ light: "", dark: "dark", orient: "" })
+  }
+  const themeLight = () => {
+    setTheme({ light: "light", dark: "", orient: "" })
+  }
+  const themeOrient = () => {
+    setTheme({ light: "", dark: "", orient: "orient" })
+  }
+
   return (
     <header>
       <h1>Scoala de soferi Nath Grigore</h1>
@@ -18,6 +33,15 @@ const Header = () => {
           </li>
           <li>
             <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <button onClick={themeLight}>Light</button>
+          </li>
+          <li>
+            <button onClick={themeDark}>Dark</button>
+          </li>
+          <li>
+            <button onClick={themeOrient}>Orient</button>
           </li>
         </ul>
       </nav>
